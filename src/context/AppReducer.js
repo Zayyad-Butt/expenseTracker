33c6185fa9b896931f03023f005ee9b6662b5ceb
Zayyad Butt <75773764+Zayyad-Butt@users.noreply.github.com>
2020-12-19 15:59:03 +0500

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,12 +1,5 @@
-"import/no-anonymous-default-export": ["error", {
-  "allowArray": false,
-  "allowArrowFunction": false,
-  "allowAnonymousClass": false,
-  "allowAnonymousFunction": false,
-  "allowCallExpression": true, // The true value here is for backward compatibility
-  "allowLiteral": false,
-  "allowObject": false
-}]
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+
 export default (state,action)=>{
     switch(action.type){
         case 'DELETE_TRANSACTION':
